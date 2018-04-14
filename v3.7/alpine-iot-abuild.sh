b84d72f95beaafbaab938d3ddc0642d7bc146e60
Yoshinori Takesako <takesako@namazu.org>
2018-04-14 07:19:26 +0000

    add build script for v3.7

---
+++ v3.7/alpine-iot-abuild.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# sudo apk update
+# sudo apk upgrade
+# sudo apk add alpine-sdk
+
+set -e
+
+PACKAGE="
+	hts_engine htsvoice-mei open_jtalk 
+	libftdi1 avrdude 
+	binutils-avr gcc-avr avr-libc 
+	micronucleus
+	php5
+	linux-iot
+	"
+
+for dir in $PACKAGE; do
+	echo "$dir: building..."
+	cd $dir
+	abuild checksum
+	abuild -r
+	cd ..
+done
+
